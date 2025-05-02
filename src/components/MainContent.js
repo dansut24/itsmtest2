02c871d90a996ba1b2884af0bcc03f8d4b43dfb3
dansut24 <Danieljamessutton18@outlook.com>
2025-05-02 06:46:55 +0100

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -10,6 +10,7 @@ const MainContent = () => {
       sx={{
         width: '100%',
         maxWidth: '100%',
+        height: '100%',
         flexGrow: 1,
         display: 'flex',
         transition: "transform 0.3s ease",
