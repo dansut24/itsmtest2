be8a540968aab7e3d7d403e16854a6927bd85d69
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 13:14:19 +0100

    Update Changes.js

--- src/pages/Changes.js
+++ src/pages/Changes.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Changes = () => {
   return (
-    <div style={ padding: 16 }>
+    <div style={{ padding: 16 }}>
       <h2>Changes Page</h2>
       <p>This is a placeholder for the Changes page.</p>
     </div>
