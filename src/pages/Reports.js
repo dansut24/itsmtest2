eb7525072252e182cf84b5607050aac91868bb38
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 13:15:41 +0100

    Update Reports.js

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Reports = () => {
   return (
-    <div style={ padding: 16 }>
+    <div style={{ padding: 16 }}>
       <h2>Reports Page</h2>
       <p>This is a placeholder for the Reports page.</p>
     </div>
