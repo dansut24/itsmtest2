02ec77533f7e53dcd0286a253fbfbbc42073968d
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 13:16:16 +0100

    Update UserProfile.js

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserProfile = () => {
   return (
-    <div style={ padding: 16 }>
+    <div style={{ padding: 16 }}>
       <h2>UserProfile Page</h2>
       <p>This is a placeholder for the UserProfile page.</p>
     </div>
