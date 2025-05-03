53e39f80142dc3ca147cc91bd6f59cdae460bcfc
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 13:12:30 +0100

    Update Assets.js

--- src/pages/Assets.js
+++ src/pages/Assets.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Assets = () => {
   return (
-    <div style={ padding: 16 }>
+    <div style={{ padding: 16 }}>
       <h2>Assets Page</h2>
       <p>This is a placeholder for the Assets page.</p>
     </div>
