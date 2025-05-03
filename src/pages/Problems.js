ba58bc50aae2285704be9cd5143415797f4eeb78
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 13:15:22 +0100

    Update Problems.js

--- src/pages/Problems.js
+++ src/pages/Problems.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Problems = () => {
   return (
-    <div style={ padding: 16 }>
+    <div style={{ padding: 16 }}>
       <h2>Problems Page</h2>
       <p>This is a placeholder for the Problems page.</p>
     </div>
