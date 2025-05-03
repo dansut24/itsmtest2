2298074e394889eedc4ca2f6eb1621b8b1f9aab1
dansut24 <Danieljamessutton18@outlook.com>
2025-05-03 14:45:36 +0100

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -9,13 +9,13 @@ const MainContent = () => {
     <Box
   sx={{
     width: '100%',
-    maxWidth: '100%',
     height: '100%',
     flexGrow: 1,
     display: 'flex',
     flexDirection: 'column',
     overflow: 'auto',
     backgroundColor: 'background.default',
+    pt: { xs: '92px', sm: '92px' }, // Responsive if needed
   }}
 >
   <Outlet />
