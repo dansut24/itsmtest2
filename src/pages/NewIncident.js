db07536548eb346740f817a2ed8096783407cacf
dansut24 <Danieljamessutton18@outlook.com>
2026-03-11 22:11:05 +0000

    Update NewIncident.js

--- src/pages/NewIncident.js
+++ src/pages/NewIncident.js
@@ -1,5 +1,5 @@
 // src/pages/NewIncident.js
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Search, User, ChevronRight, Check, AlertCircle,
